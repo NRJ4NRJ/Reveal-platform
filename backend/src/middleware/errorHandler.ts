cbbf4dc7958e9fb1f2dd33be07e2b533dad6dabd
Rififi97 <nicolas.clecoeur@gmail.com>
2026-05-04 10:42:16 +0200

    Delete backend directory

--- backend/src/middleware/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  console.error(err);
-  const status = err.status || 500;
-  const message = err.message || "Erreur interne du serveur";
-  const code = err.code || "INTERNAL_ERROR";
-  res.status(status).json({ error: message, code });
-}
